a2cf5280b65c5d103f50a8643d1ac1476432e4b4
Jose <jose.s.contacto@gmail.com>
2022-10-25 20:46:40 -0300

    added last file searcher in proyect generator

--- generate_proyect.sh
+++ generate_proyect.sh
@@ -7,6 +7,14 @@ id=0
 priority=$2
 area=$3
 deadline=$4
+thisWeek=$5
+
+#get the last file 
+
+if [ "$(ls -A $path)" ]; then
+	lastFile= ls -tp | grep -v /$ |head -1
+	echo $lastFile
+fi
 
 touch $proPath
 echo "id= "$id >> $proPath
@@ -14,3 +22,4 @@ echo "name= "$name>> $proPath
 echo "priority= "$priority >> $proPath
 echo "area= "$area >> $proPath
 echo "deadline= "$deadline >> $proPath
+echo "thisWeek= "$thisWeek >> $proPath
--- generate_task.sh
+++ generate_task.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
 path=~/gtd/tasks/
-name=$1
+todo=$1
 taskPath=$path$name".task"
 id=0
 priority=$2
-area=$3
+proyect=$3
 deadline=$4
+today=$5
 
 touch $taskPath
 echo "id= "$id >> $taskPath
-echo "name= "$name>> $taskPath
+echo "todo= "$todo >> $taskPath
 echo "priority= "$priority >> $taskPath
-echo "area= "$area >> $taskPath
+echo "proyect= "$proyect >> $taskPath
 echo "deadline= "$deadline >> $taskPath
+echo "today= "$today >> $taskPath
