3ad2b773853124c5ad8cf05005936e65ac673b7d
Jose <jose.s.contacto@gmail.com>
2022-11-14 00:52:22 -0300

    Change moving listing methods from gtd to list utility

--- gtd.sh
+++ gtd.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 source utils/format.sh
+source utils/list.sh
 
 index(){
   talk "looking for updates"
-  #git pull origin main
+  git pull origin main
   menu
 }
 
@@ -44,88 +45,6 @@ enterInput(){
   sectionHeader
   talk "insert the data"
   read data && touch inputs/"$data"
-}
-
-listData(){
-  clear
-  sectionHeader
-  talk "what kind of data do you wanna list?"
-  space 
-  echo "a) your tasks"
-  echo "b) your proyects"
-  echo "c) your inputs"
-  read -n 1 -s toList
-  if [ "$toList" == "a" ]; then listTasks;
-  elif [ "$toList" == "b" ]; then listAllProyects;
-  elif [ "$toList" == "c" ]; then listInputs;
-  else echo "wrong input. Try again"; fi;
-}
-
-listTasks(){
- space
- talk "and do you wanna list all your tasks, or only which are related to a particular proyect?"
- space
- echo "a) all"
- echo "b) all related to"
- space
- read -n 1 -s wanna
-  if [ "$wanna" == "a" ]; then listAllTasks;
-  elif [ "$wanna" == "b" ]; then listTasksRelatedProyect;
-  else echo "wrong input. Try again"; fi;
-  read a
-}
-
-listAllTasks(){
-   for FILE in tasks/*; do
-    id=$(grep -r 'id=' "$FILE")
-    todo=$(grep -r 'todo=' "$FILE") 
-    echo $id " " $todo
-  done
-}
-
-listTasksRelatedProyect(){ 
- talk "enter proyect id to see all task related to and press enter"
- space
- listAllProyects
- space
- read -s proyToSearch
- listAllTaskRelatedTo "$proyToSearch"
-}
-
-listInputs(){
-  space
-  cd inputs/
-  for n in *; do printf '%s\n' "$n"; done
-  cd ..
-  read s 
-}
-
-listAllProyects(){
-  if [[ -z "$1" ]]; then
-    for FILE in proyects/*; do
-      id=$(grep -r 'id=' "$FILE")
-      name=$(grep -r 'name=' "$FILE") 
-      echo $id " " $name
-    done
-  else
-      grep -r "id= $1" proyects/ 
-  fi
-}
-
-listAllTaskRelatedTo(){
-  proj="$(grep -l "id= $1" proyects/*)"
-  projName=$(grep -i 'name= ' $proj | sed 's/name= //g')
-  tasks="$(grep -l "proyect= $projName" tasks/*)"
-  for FILE in tasks/*; do
-    found="$(grep -l "proyect= $projName" "$FILE")"
-    if [[ ! -z "$found" ]]; then
-    for FOUND in "$found"; do 
-    idFound="$(grep -r "id=" "$found")"
-    todoFound="$(grep -r "todo=" "$found" )"
-      echo "$todoFound" "$idFound"
-    done
-  fi
-  done
 }
 
 cleanData (){
