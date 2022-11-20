bce2b01b6a0f2c3a89c747f2fcbc311503b1f242
Jose <jose.s.contacto@gmail.com>
2022-11-19 21:12:13 -0300

    Add-basic-function-declaration-into-dao-0

--- utils/crud.sh
+++ utils/crud.sh
@@ -1,2 +1,20 @@
 #!/bin/bash
-# this sh mains to be a kind of dao, or at least, be a layer between filesystem and gtd tool, centralizing and simplifying crud op
+# this sh mains to be a kind of dao, or at least, be a layer between filesystem and gtd tool, centralizing and simplifying crud ops
+# this sh depends of crud into owntools
+  
+kindof=$1
+path=$2
+
+
+function create(){
+  
+}
+
+function update(){
+  newVersion=$1
+
+}
+
+function delete(){
+  
+}
